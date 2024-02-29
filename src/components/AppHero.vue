6fe57cd5722e5986fed38b2fd2b2a8512494e1be
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-29 09:56:54 +0100

    Ottimizzazione bonus

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -5,22 +5,53 @@ export default {
 </script>
 
 <template>
-    <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi fuga soluta explicabo nihil aperiam voluptas quisquam, ducimus nesciunt sit quasi amet, eum aliquam, fugit iste blanditiis esse veniam enim nam.
-    </p>
+    <div id="jumbotron">
+        <h2>
+            Il mio jumbotron
+        </h2>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi fuga soluta explicabo nihil aperiam voluptas quisquam, ducimus nesciunt sit quasi amet, eum aliquam, fugit iste blanditiis esse veniam enim nam.
+        </p>
+        <button>
+            Cliccami
+        </button>
+    </div>
 </template>
 
 <style>
-    p{
+    #jumbotron{
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+
+        height: 600px;
+        position: relative;
+
+        padding: 20px;
+
+
+    }
+    #jumbotron p{
         color : red;
         text-align: center;
         padding: 20px;
         cursor: pointer;
     }
 
-    p:hover{
+    #jumbotron p:hover{
         text-decoration: underline;
         color : purple
         
     }
+    #jumbotron button{
+        background-color: blue;
+        color : white;
+
+        border: none;
+        border-radius: 10px;
+
+        padding: 10px;
+
+        cursor: pointer;
+        }
 </style>
